61cdb3879d6db82c8fc6d02b8622c01e7ed869d1
Peng Wei <pengwei@cs.uchicago.edu>
2019-04-06 23:20:28 -0500

    add today's work

--- hw1/pb_1.py
+++ hw1/pb_1.py
@@ -4,9 +4,10 @@ import numpy as np
 import datetime as dt
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
-MAX_ROWS = 6839451
+import matplotlib as mpl
+import geopandas as gpd
+import neighborhoods as nbhds
+import json
 
 COL_TYPES = {'block': str, 
              'case_number': str,
@@ -17,26 +18,89 @@ COL_TYPES = {'block': str,
              'longitude': float,
              'year': int,
              'ward': int}
+MAX_ROWS = 6839451 # the total rows of the original data
+
+CRIME_DATA_ID = "6zsd-86xi"
 
 def get_crime():
-	'''
-	Get 2017 and 2018 crime data from chicago data portal
+    '''
+    Get 2017 and 2018 crime data from chicago data portal
 
-	Return:
-	    pandas dataframe with the columns and dtypes as COL_TYPES
-	'''
-	cols = [item for item in COL_TYPES.keys()]
-	client = Socrata('data.cityofchicago.org',
-	                 'Lfkp6VmeW3p5ePTv0GhNSmrWh',
-	                 username="pengwei@uchciago.edu",
-	                 password="2h1m@k@1men")                     
-	res = client.get("6zsd-86xi", select=",".join(cols),
-	                 where="year = 2017 or year = 2018",
-	                 limit = MAX_ROWS)
+    Return:
+        pandas dataframe with the columns and dtypes as COL_TYPES
+    '''
+    cols = [item for item in COL_TYPES.keys()]
+    client = Socrata('data.cityofchicago.org',
+                     'Lfkp6VmeW3p5ePTv0GhNSmrWh',
+                     username="pengwei@uchciago.edu",
+                     password="2h1m@k@1men")
+    conds = "year = 2017 or year = 2018"
+    res = client.get(CRIME_DATA_ID, 
+                     select=",".join(cols),
+                     where= conds,
+                     limit = MAX_ROWS)
+    client.close()
+    df = pd.DataFrame.from_records(res)
+    df = df[df['ward'].notna()].astype(COL_TYPES)
+    df['date'] = pd.to_datetime(df['date'])
+    return df
 
-	df = pd.DataFrame.from_records(res).astype(COL_TYPES)
-	df['date'] = pd.to_datetime(df['date'])
-	return df
 
-if __name__ == '__main__':
-	go()
+def get_community():
+    '''
+    Get 77 community nums's community name from chicago data portal
+
+    Return:
+        dataframe with community num and community name
+    '''
+    client = Socrata('data.cityofchicago.org',
+                     'Lfkp6VmeW3p5ePTv0GhNSmrWh',
+                     username="pengwei@uchciago.edu",
+                     password="2h1m@k@1men")
+    res = client.get("igwz-8jzy", select = 'area_numbe, community')
+    df = pd.DataFrame.from_records(res)
+    df.rename(index = str, 
+              columns = {"area_numbe": "community_area"}, 
+              inplace = True)
+    client.close()
+    return df
+
+def link_with_neighborhoods(dataframe, lng_col, lat_col):
+    '''
+    Helper function to get a geocoded dataframe
+
+    Input:
+        df: dataframe with latitude and longtitude columns
+    Return:
+        A dataframe that is geocoded
+    '''
+    nbhd = nbhds.import_geometries(nbhds.NEIGHS_ID)
+    geodf = nbhds.convert_to_geodf(dataframe, lng_col, lat_col)
+    return nbhds.find_neighborhoods(geodf, nbhd)
+
+def map_city(geodf):
+    '''
+    map the a chicago heatmap
+
+    Input:
+        geodf: geom dataframe after link with the neighborhoods
+        nbhd: geom dataframe of 
+
+    return plot of heatmap
+    '''
+    nbhd = nbhds.import_geometries(nbhds.NEIGHS_ID)
+    first_col = geodf.columns[0]
+    fig, ax = plt.subplots(1)
+    heat = geodf.dissolve(by='pri_neigh', aggfunc='count')
+    heat = nbhd.merge(heat, on='pri_neigh', how='left').fillna(0)
+    heat.plot(ax=ax, cmap='coolwarm', column=first_col, linewidth=0.8,
+              linestyle='-')
+    ax.axis('off')
+    ax.set_title('Chicago ' + 'crime' + ' Heat Map')
+    n_min = min(heat[first_col])
+    n_max = max(heat[first_col])
+    leg = mpl.cm.ScalarMappable(cmap='coolwarm', norm=mpl.colors.Normalize(
+        vmin=n_min, vmax=n_max))
+    leg._A = []
+    colorbar = fig.colorbar(leg)
+    return plt
---
+++ hw1/pb_2.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import geopandas as gpd
+import json
+from census import Census
+from us import states
+
+
+NAMES_DIC = {'B01003_001E': "population", 
+             'B02001_002E': "population_white_alone", 
+             'B02001_003E': "population_black_alone", 
+             'B15003_021E': "education_associates", 
+             'B19001_001E': "total_income"}
+
+def get_acs_data():
+    '''
+    Get the information from census data
+    Total population, white population, black population,
+    associates degree population, household income
+
+    Return:
+        pandas dataframe
+    '''  
+    c = Census("3eb1575454b4de2cf12e0072bd946ecb852579d2")
+    res = c.acs5.get(('NAME', 
+    	       'B01003_001E',
+    	       'B02001_002E',
+    	       'B02001_003E',
+    	       'B15003_021E',
+    	       'B19001_001E'),
+               {'for': 'block group',
+               'in': 'state: {} county: {}'.format('17','031')},
+               year = 2017)
+    df = pd.DataFrame.from_records(res)
+    df.rename(columns=NAMES_DIC,inplace=True)
+    df.drop(columns =['NAME'], axis=1, inplace=True)
+    df['geoid'] = df["state"] + df["county"] + df["tract"]
+    return df
+
+def link_block(acs_df):
+	'''
+    Get blocks using API from chicago data portal
+
+    Return:
+        geo dataframe of chicago
+    '''
+	client = Socrata('data.cityofchicago.org',
+                     'Lfkp6VmeW3p5ePTv0GhNSmrWh',
+                     username="pengwei@uchciago.edu",
+                     password="2h1m@k@1men")
+    res = client.get("74p9-q2aq")
+    df = pd.DataFrame.from_records(res)
+    df.rename(index = str, 
+              columns = {"geoid10": "geoid"}, 
+              inplace = True)
+    client.close()
+	return pd.merge(df, acs_df, on='geoid')
+
+def link_crime_acs(geo_crime, geo_acs):
+	'''
+	Link two geo data frame based on the location
+	'''
+	return gpd.sjoin(geo_crime, geo_acs, how="left")
+
+
+	
+
+
