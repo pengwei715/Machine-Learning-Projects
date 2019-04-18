d8436d1da14c9ef270d7d46ca08180d4b42139ca
pgw <pengwei@uchicago.edu>
2019-04-18 15:56:16 -0500

    ready to submit

--- hw2/pipeline/evaluator.py
+++ hw2/pipeline/evaluator.py
@@ -11,63 +11,66 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 import matplotlib.pyplot as plt
 
-def pre(model, x_test):
-	'''
-	Use model to do predictions
+def pre(model, x_test, threshold):
+    '''
+    Use model to do predictions
 
-	Input:
-	    model: log model
-	    x_test: dataframe of independent variable
-	Return:
-	    A array of predictions
-	'''
-	return model.predict(x_test)
+    Input:
+        model: log model
+        x_test: dataframe of independent variable
+        threshod: float that used to do the prediction
+    Return:
+        A array of predictions
+    '''
+    pred_scores = model.predict_proba(x_test)
+    pred_label = [1 if x[1]>threshold else 0 for x in pred_scores]
+    return pred_label
+
 
 def get_accu(y_test, pres):
-	'''
-	Use model to do predictions
+    '''
+    Use model to do predictions
 
-	Input:
-	    model: log model
-	    x_test: dataframe of independent variable
-	Return:
-	    A array of predictions
-	'''
-	return metrics.accuracy_score(y_test, pres)
+    Input:
+        model: log model
+        x_test: dataframe of independent variable
+    Return:
+        A array of predictions
+    '''
+    return metrics.accuracy_score(y_test, pres)
 
 def get_report(y_test, pres):
-	'''
-	Get the report of the model
+    '''
+    Get the report of the model
 
-	Input:
-	    y_test: dependent variable's dataframe
-	    pres: np array of predictions
-	Return:
-	    report withprecision, recall, f1-score, support
-	'''
-	return classification_report(y_test, pres)
+    Input:
+        y_test: dependent variable's dataframe
+        pres: np array of predictions
+    Return:
+        report withprecision, recall, f1-score, support
+    '''
+    return classification_report(y_test, pres)
 
 
-def get_roc_auc( y_test, x_test, model):
-	'''
-	Plot the ROC curve
+def get_roc_auc( y_test, x_test, model, pres):
+    '''
+    Plot the ROC curve
 
-	Input:
-	    y_test: dependent variable's dataframe
-	    pres: np array of predictions
-	Return:
-	    ROC curve
-	'''
-	logit_roc_auc = roc_auc_score(y_test, model.predict(x_test))
-	fpr, tpr, thresholds = roc_curve(y_test,model.predict_proba(x_test)[:,1])
-	plt.figure()
-	plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
-	plt.plot([0, 1], [0, 1],'r--')
-	plt.xlim([0.0, 1.0])
-	plt.ylim([0.0, 1.05])
-	plt.xlabel('False Positive Rate')
-	plt.ylabel('True Positive Rate')
-	plt.title('Receiver operating characteristic')
-	plt.legend(loc="lower right")
-	plt.savefig('Log_ROC')
-	return plt
+    Input:
+        y_test: dependent variable's dataframe
+        pres: np array of predictions
+    Return:
+        ROC curve
+    '''
+    logit_roc_auc = roc_auc_score(y_test, pres)
+    fpr, tpr, thresholds = roc_curve(y_test,model.predict_proba(x_test)[:,1])
+    plt.figure()
+    plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    return plt
